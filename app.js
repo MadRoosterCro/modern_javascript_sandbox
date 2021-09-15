f1d828fd44772121720415368acf6db454babdc8
MadRoosterCro <dario.car.kz@gmail.com>
2021-09-15 22:55:06 +0200

    branch naming issues

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // Log to console instead
 // btw these are inline comments
 
-console.log('Hello world')
-console.log(123)
-console.log(true)
-const greeting = 'Hello world!'
-console.log(greeting)
-console.log([1,2,3,4])
-console.log({a:1, b:'something', c:true, d:{e:1, f:'foo', g:false}})
-console.table({a:1, b:2, c:true, d:'tralalalal'})
+console.log('Hello world');
+console.log(123);
+console.log(true);
+const greeting = 'Hello world!';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 'something', c: true, d: { e: 1, f: 'foo', g: false } });
+console.table({ a: 1, b: 2, c: true, d: 'tralalalal' });
 
-console.error('This is some error, it is very useful')
-console.warn(`This is a warning, it is very useful`)
-console.time('Hello')
-console.log('jo jo jo')
-console.log('jo jo jo')
-console.log('jo jo jo')
-console.log('jo jo jo')
-console.log('jo jo jo')
-console.timeEnd('Hello')
+console.error('This is some error, it is very useful');
+console.warn(`This is a warning, it is very useful`);
+console.time('Hello');
+console.log(' Hello world ');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.timeEnd('Hello');
+
 /*
 and these are 
 multiline 
