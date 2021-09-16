ce2d2688f62c34947c7d6c6c5493e2a7aaefbe34
MadRoosterCro <dario.car.kz@gmail.com>
2021-09-16 20:29:22 +0200

    readme sorted

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-/*
-there are several ways to declare a variable
-
-var ===> the "old" way of declaring a variable, not used very often and is considered archaic
-const, let ===> the "new" way of declaring variables
+/* please follow along the readme
 
 first we will cover VAR:
 */
@@ -19,39 +15,23 @@ console.log(hello); // ==> this will actually show as undefined in console
 hello = 'hello';
 console.log(hello); // ==> this will now show a value in console, we assigned it a string value
 
-/* 
-variables can include only numbers, letters, _, $
-varables can not start with a number
-
-1hello = 'hello'; 
-
-this will throw a syntax error
-for start let's just use letters
-*/
-
-// for variables with multiple words, we use camelCase,
-// it is the most readable and it looks like this:
+// Camel case and other examples:
 
 var firstName = 'Dario'; // Camel case, we will mostly be using this for regular variables
 var first_name = 'Dario'; // Underscore
 var FirstName = 'Dario'; // Pascal case
 var firstname; // =====> unreadable
 
-/* declaring variables using LET
- we use let for non-constant variables,
- variables that will change later in the code
-*/
+// declaring variables using LET
+
 let someName;
-console.log(someName); // again, this will be undefined at this moment as it only initialized
+console.log(someName); // again, this will be undefined at this moment as it is only initialized
 someName = 'John';
 console.log(someName);
 someName = 'Johnny';
 console.log(someName);
 
-/* declaring variables using CONST
- we use const for constant variables,
- variables that can not be reassigned
-*/
+// declaring variables using const
 const lastName = 'Malkovich';
 console.log(lastName); // works just fine, as we can see it in the console
 
@@ -63,32 +43,4 @@ we must assing a value to it.
 
 const lastName; 
 This will throw a SyntaxError
-
-!!!!!!!!!! IMPORTANT NOTE: 
-later we will mention object mutation, sometimes people get
-confused because we can mutate(change) data inside an object, 
-but we cannot reassign the object itself
-example:
-
-const person = {
-    name: 'John',
-    age: 50,
-}
-now when we console.log(person) it will show us
-{name: 'John', age:50}
-
-now if we mutate the data inside like this
-person.name = 'Janette';
-person.age = 30;
-and then we console.log(person) it will show us the following
-{name: 'Janette', age:30}
-
-we did not actually reassign the person, we just mutated the data inside itself
-same thing applies for arays
-
-more on this later on, let's keep it light for now
-
-every developer has their own preference when declaring variables;
-let and const became the standard as it makes your code more readable
-and robust
 */
