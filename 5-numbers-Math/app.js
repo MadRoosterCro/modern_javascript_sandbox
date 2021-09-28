b1c67fe2a64beddfbb1195e2c31243ad53ba05e4
MadRoosterCro <dario.car.kz@gmail.com>
2021-09-28 11:23:37 +0200

    ext app.js

--- 5-numbers-Math/app.js
+++ 5-numbers-Math/app.js
@@ -1,3 +1,20 @@
 const num1 = 100;
 const num2 = 50;
 let val;
+
+// Simple math with numbers
+val = num1 + num2;
+
+// try it with different operators/operations written bellow
+
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+
+// Since Math is an object and that means it has properties and some methods
+// Methods are some functions inside that Math object
+console.log(val);
