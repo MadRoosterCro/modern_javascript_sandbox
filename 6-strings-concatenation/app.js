fa2cee34023942e129a8de6a8aaccfd983741880
MadRoosterCro <dario.car.kz@gmail.com>
2021-09-30 14:37:34 +0200

    lesson 6 extended

--- 6-strings-concatenation/app.js
+++ 6-strings-concatenation/app.js
@@ -5,4 +5,9 @@ let val;
 
 // lets see what will happen if we add the declared variables
 val = firstName + lastName;
-console.log(val);
+
+console.log(`If we just add variables together we get ===>`, val);
+// we can see that this just smushes the strings together, this is why we need concatenation
+
+let val1 = firstName + " " + lastName;
+console.log(`Now we added a space between the variables and we got ===>`, val1);
