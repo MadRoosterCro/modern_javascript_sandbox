8c46184b359c50b9bba4fa5d2b659b5db311378b
MadRoosterCro <dario.car.kz@gmail.com>
2021-09-30 14:44:02 +0200

    lesson 6 extended

--- 6-strings-concatenation/app.js
+++ 6-strings-concatenation/app.js
@@ -11,3 +11,11 @@ console.log(`If we just add variables together we get ===>`, val);
 
 let val1 = firstName + " " + lastName;
 console.log(`Now we added a space between the variables and we got ===>`, val1);
+
+// if we want to add something to a variable, we need to append it
+
+let val2 = "John "; // -> notice there is a space after the name
+// now if we do val2 = "Malkovich", it will overwrite the initial value, so for appending it we can do
+val2 += "Malkovich";
+
+console.log(`With the += for appending variables we got ====>`, val2);
