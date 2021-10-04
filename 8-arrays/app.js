58cd897c5109081db300ba060410d31f5795eadb
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-04 16:25:12 +0200

    tryin to explain arrays

--- 8-arrays/app.js
+++ 8-arrays/app.js
@@ -25,3 +25,17 @@ console.log(val1);
 let indexValue = 3; // remember that index starts from 0, so index 3 is actually 4th position in the array
 let val2 = numbers[indexValue]; // we pass the index value in the [] brackets
 console.log(`The value on index ${indexValue} is`, val2); // and we confirm that 54 is on index 3
+
+// insert something in the array
+let newValueIndex = 2; // pick what index we want to change
+let val3 = (numbers[newValueIndex] = 100); // set the value at index we picked to the new desired value
+console.log(`We just set the value at index ${newValueIndex} to`, val3);
+console.log(numbers); // in the console you can now see we mutated the original array
+
+// find index of a value
+let findTheIndexOfThisValue = 12; // here we pick a value that we know it exists in our array numbers
+let val4 = numbers.indexOf(findTheIndexOfThisValue); // find the index of the picked value
+console.log(
+  `We can now see that the index of value ${findTheIndexOfThisValue} is`,
+  val4
+);
