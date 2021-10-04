2600ae38f042c4cdf59fa4decc1ac666fc4cd390
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-04 14:50:27 +0200

    ls 8 ext

---
+++ 8-arrays/app.js
@@ -0,0 +1,8 @@
+// Creating some arrays
+const numbers = [54, 87, 65, 54, 12, 32, 22, 5];
+
+// we can create arrays in multiple ways
+const numbers2 = new Array(22, 33, 44, 55, 66);
+
+// arrays do not have to be just numbers
+const fruit = ["Apple", "Orange", "Pear", "Fig"];
