65416c3b4cfd439fad605913638888e409130d4c
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-04 14:52:47 +0200

    ls 8 ext

--- 8-arrays/app.js
+++ 8-arrays/app.js
@@ -6,3 +6,7 @@ const numbers2 = new Array(22, 33, 44, 55, 66);
 
 // arrays do not have to be just numbers
 const fruit = ["Apple", "Orange", "Pear", "Fig"];
+
+// we can also mix types in an array
+const mixed = [22, "Hey", true, undefined, null, { a: 1, b: 2 }, new Date()];
+console.log(`Our mixed array looks like this: "${mixed}"`); // let's see what it looks like in the console
