121d82e58c46ae649583f0ba807ad27466e6df61
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-11 12:20:24 +0200

    ext app.js on ls 9

--- 9-object-literals/app.js
+++ 9-object-literals/app.js
@@ -9,6 +9,10 @@ const person = {
 let val = person;
 console.log(val);
 
-// If we want to get the specific value from the person object
+// If we want to get the specific value from the person object, this is the recommended way
 let val1 = person.firstName;
 console.log(`First name of our object is `, val1);
+
+// we can do the same thing using brackets []
+let val2 = person['lastName'];
+console.log(`Last name of our object is `, val2);
