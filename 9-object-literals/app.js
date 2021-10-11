dfcb8ba554a890af6d120a11da90aa891c4de214
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-11 12:31:59 +0200

    ext app.js on ls 9

--- 9-object-literals/app.js
+++ 9-object-literals/app.js
@@ -3,7 +3,7 @@ const person = {
   lastName: 'Malkovich',
   age: 67,
   email: 'example@example.com',
-  hobbies: ['music, acting, sports'],
+  hobbies: ['music', 'acting', 'sports'],
   address: {
     city: 'Boston',
     state: 'Massachusetts',
@@ -26,3 +26,13 @@ let val2 = person['lastName'];
 console.log(`Last name of our object is `, val2);
 
 // let's log out each prop in the person object to see different outputs
+let val3 = person.age;
+console.log(`The age is `, val3);
+let val4 = person.email;
+console.log(`The email is `, val4);
+let val5 = person.hobbies[1]; // we can use brackets to see array members by index
+console.log(`The value at index 1 is `, val5);
+let val6 = person.address.state; // notice we can just keep accessing nested objects with dot (.)
+console.log(`The prop(state) value in our nested object is `, val6);
+let val7 = person.getBirthYear();
+console.log(`The year of birth of the person is ${val7}`, val7);
