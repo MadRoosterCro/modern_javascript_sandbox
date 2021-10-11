4d02ab4a281e62674ec17578f12bce3355e81fb4
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-11 12:18:37 +0200

    ext app.js on ls 9

--- 9-object-literals/app.js
+++ 9-object-literals/app.js
@@ -1,9 +1,14 @@
 const person = {
   firstName: 'John',
   lastName: 'Malkovich',
+  age: 67,
+  city: 'Boston',
 };
 
-let val;
-
-val = person;
+// lets see the console output
+let val = person;
 console.log(val);
+
+// If we want to get the specific value from the person object
+let val1 = person.firstName;
+console.log(`First name of our object is `, val1);
