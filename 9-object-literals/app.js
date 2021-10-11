57cda5c107759abd18abede02f0ef324466e63fc
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-11 12:24:03 +0200

    ext app.js on ls 9

--- 9-object-literals/app.js
+++ 9-object-literals/app.js
@@ -2,7 +2,15 @@ const person = {
   firstName: 'John',
   lastName: 'Malkovich',
   age: 67,
-  city: 'Boston',
+  email: 'example@example.com',
+  hobbies: ['music, acting, sports'],
+  address: {
+    city: 'Boston',
+    state: 'Massachusetts',
+  },
+  getBirthYear: function () {
+    return 1953;
+  },
 };
 
 // lets see the console output
@@ -16,3 +24,5 @@ console.log(`First name of our object is `, val1);
 // we can do the same thing using brackets []
 let val2 = person['lastName'];
 console.log(`Last name of our object is `, val2);
+
+// let's log out each prop in the person object to see different outputs
