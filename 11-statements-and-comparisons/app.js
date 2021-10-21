c065ee2d2fceaef4fdca4cd4376dbd536488bac6
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-21 10:57:34 +0200

    adding testing

--- 11-statements-and-comparisons/app.js
+++ 11-statements-and-comparisons/app.js
@@ -14,8 +14,9 @@ if (id != 101) {
   console.log(`Comparison is not equal to ${id}`);
 }
 
-// STRICT comparison
+// STRICT equality comparison - means that we will compare value and the type
 const newId = 200; // this is a number
+const newIdString = '200'; // this is a number
 
 if (newId === 200) {
   // when we use triple equal sign, we compare value and the type, so it is more strict!
@@ -23,3 +24,22 @@ if (newId === 200) {
 } else {
   console.log('INCORRECT!');
 }
+
+// STRICT inequality comparison
+// we will compare number to a string, so we expect it to not be equal
+if (newIdString === 200) {
+  console.log(`Comparison is equal to ${newIdString}!`); // this is not correct, so we move to forward to the else part
+} else {
+  console.log(`Comparison is not equal to ${newIdString}!`);
+}
+
+// it is always better to use triple equality
+
+// Testing if a variable is defined
+
+if (typeof id !== 'undefined') {
+  console.log(`The ID is defined and it is: ${id}`);
+} else {
+  console.log(`The ID is not defined!`);
+}
+
