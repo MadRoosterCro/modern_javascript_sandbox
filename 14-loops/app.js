bf23a8cf3986d27612ced2f2efa4592dc5fe9fbd
MadRoosterCro <dario.car.kz@gmail.com>
2021-10-26 16:18:13 +0200

    done for loop on app.js for ls 14

--- 14-loops/app.js
+++ 14-loops/app.js
@@ -1,9 +1,9 @@
 // FOR loop
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 6; i++) {
   // i++ just means we are incrementing a variable, in this case i = i+1
-  console.log(`Loop is at number ${i}`);
-} // the console prints 0,1,2,...,9
+  console.log(`L loop is at number => ${i}`);
+} // the console prints 0,1,2,...,5
 
 /** a quick explanation of the flow from the stated example
  * first we declare a variable "i" and set it's initial value at 0
@@ -14,11 +14,12 @@ for (let i = 0; i < 10; i++) {
 // if we want to create a special case on one of the iterations:
 
 for (let x = 0; x <= 10; x++) {
+  // notice now we will print 10 as well
   if (x === 8) {
     console.log('8 is the best number!');
     continue; // with this we tell the loop to stop and go to the next iteration
   }
-  console.log(`This loop is at number ${x}`);
+  console.log(`X loop is at number => ${x}`);
 }
 // without the continue keyword, our special case would be fired and the console would print
 // This loop is at number 7
@@ -26,3 +27,20 @@ for (let x = 0; x <= 10; x++) {
 // This loop is at number 8
 
 // and we want to skip that iteration because we already covered our special case with an if statement.
+
+// how to break out of the loop
+
+for (let k = 0; k < 10; k++) {
+  if (k === 2) {
+    console.log(`I don't really like the number 2`);
+    continue;
+  }
+  if (k === 7) {
+    console.log(`Stop the loop because we are at number 7`);
+    break;
+  }
+  console.log(`K loop is at number => ${k}`);
+}
+// we can see that we stopped iterating when we reach number 7 because we specified a break point there
+
+
